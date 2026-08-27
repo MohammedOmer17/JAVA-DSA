0207822aace54e454df74b0046b7d6d73427d01f
MohammedOmer17 <mdomersiddiqui0001@gmail.com>
2026-08-27 15:56:43 +0530

    initial commit

---
+++ loops/patterns/square.java
@@ -0,0 +1,14 @@
+package patterns;
+
+public class square {
+    public static void main(String args[]){
+        int rows = 4;
+        int columns = 4;
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<columns;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
